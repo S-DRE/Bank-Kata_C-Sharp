e7849073aece7a90dcd54f901ca0e3ebef7a1f29
S-DRE <sdreworks@gmail.com>
2023-12-11 11:48:04 +0100

    Back to square one!

--- Bank/Bank.cs
+++ Bank/Bank.cs
@@ -4,24 +4,6 @@ public static class Bank
 {
     static void Main(string[] args)
     {
-        /*
-        * Date       || Amount || Balance
-          29/05/2023 || -500   || 2500
-          29/05/2023 || 2000   || 3000
-          29/05/2023 || 1000   || 1000
-        */
-
-        var console = new ProConsole();
-        var dateCreator = new DateCreator();
-        var walletInMemoryRepo = new WalletInMemory();
-
-        
-        var account = new Account(console, dateCreator, walletInMemoryRepo);
-
-        
-        account.Deposit(1000);
-        account.Deposit(2000);
-        account.Withdraw(500);
-        account.PrintStatement();
+        throw new NotImplementedException();
     }
 }
---
+++ BankTests/Acceptance/BankShould.cs
@@ -0,0 +1,13 @@
+﻿using Bank;
+using Xunit;
+
+namespace BankTest.Acceptance;
+
+public class BankShould
+{
+    [Fact]
+    public void EndUpPrintingAllTheMovementsWithTheirCurrentBalance()
+    {
+        throw new NotImplementedException();
+    }
+}
