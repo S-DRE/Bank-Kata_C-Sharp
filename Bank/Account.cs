4e1ccbcfdd1cd84d81d7778ccf3093b983770908
S-DRE <sdreworks@gmail.com>
2023-12-12 17:28:46 +0100

    Added more skeleton

---
+++ Bank/Account.cs
@@ -0,0 +1,19 @@
+﻿namespace Bank;
+
+public class Account : AccountService
+{
+    public void deposit(int amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void withdraw(int amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void printStatement()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Bank/AccountService.cs
@@ -0,0 +1,8 @@
+﻿namespace Bank;
+
+public interface AccountService
+{
+    void deposit(int amount);
+    void withdraw(int amount);
+    void printStatement();
+}
