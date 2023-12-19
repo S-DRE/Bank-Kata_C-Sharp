61a22d066f70e6f50289b60f8077add8f2dbc908
S-DRE <sdreworks@gmail.com>
2023-12-19 16:31:12 +0100

    RED: RetrieveDateWhenAsked - MovementShould

---
+++ BankTests/Unit/MovementShould.cs
@@ -0,0 +1,21 @@
+﻿using Bank;
+using Xunit;
+
+namespace BankTest.Unit;
+
+public class MovementShould
+{
+
+    private readonly Movement movement;
+    
+    public MovementShould()
+    {
+        movement = new Movement(DateOnly.Parse("14/12/2012"), 1000, 5000);
+    }
+    
+    [Fact]
+    public void RetrieveDateWhenAsked()
+    {
+        Assert.Equal(DateOnly.Parse("14/12/2012"), movement.GetDate());
+    }
+}
