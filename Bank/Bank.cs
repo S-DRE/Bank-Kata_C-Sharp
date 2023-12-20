1a166bcf531b6cf11dafe6968f6984d8cc370345
S-DRE <sdreworks@gmail.com>
2023-12-20 10:36:39 +0100

    Added main execution code

--- Bank/Bank.cs
+++ Bank/Bank.cs
@@ -4,6 +4,24 @@ public static class Bank
 {
     static void Main(string[] args)
     {
-        throw new NotImplementedException();
+        /*
+       * Date       || Amount || Balance
+         14/01/2012 || -500   || 2500
+         14/01/2012 || 2000   || 3000
+         14/01/2012 || 1000   || 1000
+       */
+
+        var consolePrinter = new ConsolePrinter();
+        var cashSafe = new CashSafe();
+        var movementRepo = new MovementRepository();
+
+        
+        var account = new Account(cashSafe, consolePrinter, movementRepo);
+
+        
+        account.Deposit(1000);
+        account.Deposit(2000);
+        account.Withdraw(500);
+        account.PrintStatement();
     }
 }
