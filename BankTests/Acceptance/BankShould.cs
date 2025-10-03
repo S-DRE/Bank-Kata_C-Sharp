c509dc9140ab332a8acb9c3aa12e2eb8d5230f59
Andrés Alonso <64490259+S-DRE@users.noreply.github.com>
2025-10-03 12:01:10 +0200

    Update BankShould.cs

--- BankTests/Acceptance/BankShould.cs
+++ BankTests/Acceptance/BankShould.cs
@@ -1,4 +1,4 @@
-﻿using Bank;
+using Bank;
 using Bank.Helpers;
 using Xunit;
 
@@ -16,20 +16,27 @@ public class BankShould
         CashSafe cashSafe = new CashSafe();
         ConsolePrinter consolePrinter = new ConsolePrinter();
         MovementRepository movementRepository = new MovementRepository();
+        
+        List<DateOnly> testingDates = new List<DateOnly>
+        {
+            new (2010, 1, 10),
+            new (2012, 1, 12),
+            DateOnly.Parse("14/01/2012", GlobalVars.CULTURE_INFO)
+        };
 
         List<IMovement> expectedMovements = new()
         {
-            new Movement(DateOnly.Parse("14/01/2012", GlobalVars.CULTURE_INFO), -500, 2500),
-            new Movement(DateOnly.Parse("14/01/2012", GlobalVars.CULTURE_INFO), 2000, 3000),
-            new Movement(DateOnly.Parse("14/01/2012", GlobalVars.CULTURE_INFO), 1000, 1000)
+            new Movement(testingDates[2], -500, 2500),
+            new Movement(testingDates[1], 2000, 3000),
+            new Movement(testingDates[0], 1000, 1000)
         };
         
         account = new Account(cashSafe, consolePrinter, movementRepository);
         
         // When
-        account.Deposit(1000);
-        account.Deposit(2000);
-        account.Withdraw(500);
+        account.Deposit(testingDates[0],1000);
+        account.Deposit(testingDates[1],2000);
+        account.Withdraw(testingDates[2],500);
         
         // TODO: Find a way to assert the print since it is void
         account.PrintStatement();
@@ -38,6 +45,6 @@ public class BankShould
         Assert.Equal(2500, cashSafe.GetBalance());
         Assert.Equivalent(expectedMovements, movementRepository.GetMovements());
 
-        Assert.True(movementRepository.GetMovements().SequenceEqual(expectedMovements));
+        // Assert.True(movementRepository.GetMovements().SequenceEqual(expectedMovements));
     }
 }
