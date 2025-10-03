b79b11a12a467313244729f040a1986084f54c92
Andrés Alonso <64490259+S-DRE@users.noreply.github.com>
2025-10-03 11:59:56 +0200

    Update Account.cs

--- Bank/Account.cs
+++ Bank/Account.cs
@@ -1,4 +1,4 @@
-﻿using Bank.Helpers;
+using Bank.Helpers;
 
 namespace Bank;
 
@@ -9,7 +9,6 @@ public class Account : AccountService
     private readonly IMovementRepository movementRepository;
     
     private const string HEADER = "Date       || Amount || Balance";
-    private const string fixedDateForKataPurposes = "14/01/2012";
     
     public Account(ICashSafe cashSafe, IConsolePrinter consolePrinter, IMovementRepository movementRepository)
     {
@@ -18,15 +17,15 @@ public class Account : AccountService
         this.movementRepository = movementRepository;
     }
 
-    public void Deposit(int amount)
+    public void Deposit(DateOnly date, int amount)
     {
-        movementRepository.AddMovement(DateOnly.Parse(fixedDateForKataPurposes, GlobalVars.CULTURE_INFO), amount, cashSafe.GetBalance()+amount);
+        movementRepository.AddMovement(date, amount, cashSafe.GetBalance()+amount);
         cashSafe.AddCash(amount);
     }
 
-    public void Withdraw(int amount)
+    public void Withdraw(DateOnly date, int amount)
     {
-        movementRepository.AddMovement(DateOnly.Parse(fixedDateForKataPurposes, GlobalVars.CULTURE_INFO), amount * -1, cashSafe.GetBalance()-amount);
+        movementRepository.AddMovement(date, amount * -1, cashSafe.GetBalance()-amount);
         cashSafe.RemoveCash(amount);
     }
 
